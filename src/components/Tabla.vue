9a8a9365896bfd4effd6f3e6ffcb28ce723b759c
Joscelyn <joscelyn.covarrubias@uabc.edu.mx>
2023-06-05 23:21:23 -0700

    Meta1.3
    CRUD

--- src/components/Tabla.vue
+++ src/components/Tabla.vue
@@ -1,35 +1,225 @@
 <template>
-  <v-table>
-    <thead>
-      <tr>
-        <th class="text-left">Autor</th>
-        <th class="text-left">Titulo</th>
-        <th class="text-left">Post</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(item, index) in datos" :key="item.id">
-        <td>{{ autores[item.userId - 1].name }}</td>
-        <td>{{ item.title }}</td>
-        <td>{{ item.body }}</td>
-      </tr>
-    </tbody>
-  </v-table>
+  <v-data-table :headers="headers" :items="datos" class="elevation-1">
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>My CRUD</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ props }">
+            <v-btn color="primary" dark class="mb-2" v-bind="props">New Item</v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.autor"
+                      label="Author name"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.titulo"
+                      label="Title"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.post" label="Post"></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue-darken-1" variant="text" @click="close"> Cancel </v-btn>
+              <v-btn color="blue-darken-1" variant="text" @click="save"> Save </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">¿Estas seguro que deseas borrar?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue-darken-1" variant="text" @click="closeDelete">
+                Cancel
+              </v-btn>
+              <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">
+                Ok
+              </v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+
+    <template v-slot:item.actions="{ item }">
+      <v-icon
+        size="small"
+        class="me-2"
+        @click="editItem(item.raw)"
+        icon="mdi-pencil"
+      ></v-icon>
+      <v-icon
+        size="small"
+        class="me-2"
+        @click="deleteItem(item.raw)"
+        icon="mdi-delete"
+      ></v-icon>
+    </template>
+
+    <template v-slot:no-data>
+      <v-btn color="primary" @click="initialize">Reset</v-btn>
+    </template>
+  </v-data-table>
 </template>
 
-<script setup>
-import { ref, onMounted } from "vue";
-const datos = ref([]);
-onMounted(async () => {
-  const r = await fetch("https://jsonplaceholder.typicode.com/users/1/posts");
-  datos.value = await r.json();
-  console.log(datos.value);
-});
+<script>
+import { ref, onBeforeMount } from "vue";
 
-const autores = ref([]);
-onMounted(async () => {
-  const a = await fetch("https://jsonplaceholder.typicode.com/users");
-  autores.value = await a.json();
-  console.log(autores.value);
-});
+export default {
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      {
+        title: "Autor",
+        align: "start",
+        sortable: false,
+        key: "autor",
+      },
+      { title: "Titulo", sortable: false, key: "titulo" },
+      { title: "Post", sortable: false, key: "post" },
+      { title: "Actions", key: "actions", sortable: false },
+    ],
+
+    datos: [],
+    editedIndex: -1,
+    editedItem: {
+      autor: "",
+      titulo: "",
+      post: "",
+    },
+    defaultItem: {
+      autor: "",
+      titulo: "",
+      post: "",
+    },
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      const datosJS = ref([]);
+      const autores = ref([]);
+
+      onBeforeMount(async () => {
+        const r = await fetch("https://jsonplaceholder.typicode.com/users/1/posts");
+        datosJS.value = await r.json();
+        console.log(datosJS.value);
+
+        const a = await fetch("https://jsonplaceholder.typicode.com/users");
+        autores.value = await a.json();
+        //console.log(autores.value);
+
+        this.datos = datosJS.value.map((element) => ({
+          autor: autores.value[element.userId - 1].name,
+          titulo: element.title,
+          post: element.body,
+        }));
+      });
+
+      //[
+      // {
+      //   autor: "Yo",
+      //   titulo: "My Hero Academia",
+      //   post: "djfhbgkjydfjhadfjga",
+      // },
+      // {
+      //   autor: "Deku",
+      //   titulo: "My Hero Academia",
+      //   post: "Hola",
+      // },
+      // {
+      //   autor: "Cola",
+      //   titulo: "YAMEROOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+      //   post: "AYUDENME PORFAVBOR",
+      // },
+      // {
+      //   autor: "Taylor Swift",
+      //   titulo: "'tis the damn season",
+      //   post: "and it always leads to you in my hometown",
+      // },
+      //];
+    },
+
+    editItem(item) {
+      this.editedIndex = this.datos.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.datos.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+
+    deleteItemConfirm() {
+      this.datos.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.datos[this.editedIndex], this.editedItem);
+      } else {
+        this.datos.push(this.editedItem);
+      }
+      this.close();
+    },
+  },
+};
 </script>
